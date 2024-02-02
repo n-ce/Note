4a0de43461b5c44bf71c04ecbd5259ae4e7b3322
n-ce <abrocks1120@gmail.com>
2024-02-02 18:45:18 +0530

    finished rewrite

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,26 @@
+const textarea = <HTMLTextAreaElement>document.querySelector('textarea');
 
+textarea.value = localStorage.getItem('key') || '';
+textarea.addEventListener('input', () => {
+  localStorage.setItem('key', textarea.value);
+});
+
+document.getElementById('clear')?.addEventListener('click', () => {
+  localStorage.removeItem('key');
+  textarea.textContent = '';
+});
+
+document.getElementById('download')?.addEventListener('click', () => {
+  const textToBLOB = new Blob([textarea.value], { type: 'text/plain' });
+  const link = document.createElement('a');
+  link.download = 'Note.txt';
+  link.href = URL.createObjectURL(textToBLOB);
+  link.click();
+});
+
+const upload = <HTMLInputElement>document.getElementById('upload');
+
+upload.addEventListener('change', async () => {
+  textarea.value = await (<FileList>upload.files)[0].text();
+  localStorage.setItem('key', textarea.value);
+})
